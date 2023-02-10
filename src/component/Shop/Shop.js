28e51bc78d3075a0236fb683c7b36f9befc178e9
Majbahia <majbahia@gmail.com>
2023-02-11 01:06:40 +0600

    connect api and display all product

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProduct ]= useState([]);
+    useEffect( ()=>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    }, [])
+    const handleAddToCart = (product)=>{
+        console.log(product)
+    }
     return (
-        <div>
-            
+        <div className='shop-container'>
+            <div className="products-container">
+            {
+                products.map(product=> <Product
+                    handleAddToCart={handleAddToCart}
+                    product={product}
+                     key={product.id}
+                     ></Product>)
+            }
+            </div>
+            <div className="cart-container">
+            <h4>Order Summary</h4>
+            </div>
         </div>
     );
 };
